9aff00a26998c43c072548805dab5ca0bc4329ba
radiophonicaonline <spotradial.rph@gmail.com>
2025-04-11 08:49:45 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,27 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+// Importa Firebase scripts para el service worker
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
-const firebaseConfig = {
+// Tu configuración de Firebase
+firebase.initializeApp({
   apiKey: "AIzaSyAC-9LQSlelDVx27wd2DPxisi4M-lRwtrk",
   authDomain: "contador-de-personas-5f8aa.firebaseapp.com",
   projectId: "contador-de-personas-5f8aa",
   storageBucket: "contador-de-personas-5f8aa.appspot.com",
   messagingSenderId: "1063230344919",
   appId: "1:1063230344919:web:ed86a937ba176dddb1aa92"
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 const messaging = firebase.messaging();
 
+// Muestra notificación personalizada cuando llegue un mensaje push
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Mensaje recibido en segundo plano:', payload);
-  const notificationTitle = payload.notification.title;
+  console.log('[firebase-messaging-sw.js] Recibido en background: ', payload);
+
+  const notificationTitle = payload.notification.title || "📻 Radiophonica Online";
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/logo.png'
+    body: payload.notification.body || "¡Estamos en vivo!",
+    icon: "https://radiophonicaonline.github.io/streaming/logo.png"
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
