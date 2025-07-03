b3b2948593f24d0a4b4b3fd39aa8b20cdb8eb2d8
radiophonicaonline <spotradial.rph@gmail.com>
2025-07-03 18:42:58 -0400

    Update main-chat-color.js

--- scripts/main-chat-color.js
+++ scripts/main-chat-color.js
@@ -201,12 +201,12 @@ function actualizarContenido() {
 
   const iframe = document.getElementById("iframeCancion");
   if (iframe) {
-    iframe.src = "https://clinton-recorded-copies-logs.trycloudflare.com/nowplaying.txt?t=" + timestamp;
+    iframe.src = "https://jail-sandy-emails-ottawa.trycloudflare.com/nowplaying.txt?t=" + timestamp;
   }
 
   const portada = document.getElementById("portadaCancion");
   if (portada) {
-    portada.src = "https://clinton-recorded-copies-logs.trycloudflare.com/artwork.png?t=" + timestamp;
+    portada.src = "https://jail-sandy-emails-ottawa.trycloudflare.com/artwork.png?t=" + timestamp;
   }
 }
 
