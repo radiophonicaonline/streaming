59531a32c296ed7b6a79f91f9a6f4c2e027cb3cb
radiophonicaonline <spotradial.rph@gmail.com>
2025-07-24 08:21:04 -0400

    Update main-chat-color.js

--- scripts/main-chat-color.js
+++ scripts/main-chat-color.js
@@ -176,12 +176,12 @@ function actualizarContenido() {
 
   const iframe = document.getElementById("iframeCancion");
   if (iframe) {
-    iframe.src = "https://bradley-occasionally-liberty-journalist.trycloudflare.com/nowplaying.txt?t=" + timestamp;
+    iframe.src = "https://besides-outline-reid-gallery.trycloudflare.com/nowplaying.txt?t=" + timestamp;
   }
 
   const portada = document.getElementById("portadaCancion");
   if (portada) {
-    portada.src = "https://bradley-occasionally-liberty-journalist.trycloudflare.com/artwork.png?t=" + timestamp;
+    portada.src = "https://besides-outline-reid-gallery.trycloudflare.com/artwork.png?t=" + timestamp;
   }
 }
 
